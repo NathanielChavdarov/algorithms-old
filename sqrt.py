9ef21760f99d39d0a7a5f56971ac2ccd8f4be7eb
Nathaniel <yuzki13246@gmail.com>
2020-08-15 11:00:16 +0100

    Add square root and test file

---
+++ sqrt.py
@@ -0,0 +1,37 @@
+def sqrt(n: float) -> float:
+    if n < 0:
+        raise ValueError("Cannot find square root of a negative number!")
+    if n == 0 or n == 1:
+        return n
+
+    if int(n) == n:
+        for i in range(1, n):
+            if i*i == n:
+                print(f"The number {n} is a square number")
+                return i
+            elif i*i > n:
+                break
+    print(f"The square root of {n} is not an integer!")
+    if n < 1:
+        lbound = n
+        ubound = 1
+    else:
+        lbound = 1
+        ubound = n
+    count = 0
+    while True:
+        count += 1
+        mpoint = (ubound+lbound)/2
+        print(f"lbound = {lbound}, ubound = {ubound}, mpoint = {mpoint}")
+        if mpoint*mpoint > n:
+            ubound = mpoint
+        elif mpoint*mpoint == n:
+            return mpoint
+        else:
+            lbound = mpoint
+        if count > 30:
+            return mpoint
+
+
+if __name__ == "__main__":
+    print(sqrt(0.5))
---
+++ test_sqrt.py
@@ -0,0 +1,39 @@
+import math
+
+import sqrt as s
+
+
+def test_sqrt():
+    tolerance = 0.00000001
+
+    test_numbers = [
+        0,
+        1,
+        4,
+        9,
+        16,
+        25,
+        36,
+        49,
+        64,
+        81,
+        100,
+        # ok, now for the difficult ones
+        2,
+        5,
+        0.1,
+        0.5,
+        0.9,
+    ]
+    for n in test_numbers:
+        got = s.sqrt(n)
+        expected = math.sqrt(n)
+        if got == expected:
+            # exact match. probably a square number.
+            continue
+
+        # check with tolerance
+        lbound = expected * (1.0 - tolerance)
+        ubound = expected * (1.0 + tolerance)
+        assert lbound < got and got < ubound, \
+            f"Calculating sqrt({n}), failed: {lbound} < {got} < {ubound}"
